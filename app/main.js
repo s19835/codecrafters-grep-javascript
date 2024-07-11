88035d3c7516bac0880946aa0ea9dcfcb706e58c
s19835 <s19835@sci.pdn.ac.lk>
2024-07-11 13:22:53 +0530

    add parsePattern func

--- app/main.js
+++ app/main.js
@@ -19,12 +19,41 @@ function matchPattern(inputLine, pattern) {
 }
 
 //function to parse the pattern into a list of tokens representing character classes or literals.
+function parsePattern(pattern) {
+  const tokens = [];
+  let i = 0;
 
+  while (i < pattern.length) {
+    if (pattern[i] === '\\') {
+      if (pattern[i+1]) {
+        tokens.push('\\' + pattern[i+1]);
+        i += 2;
+      }
+      else throw new Error(`Invalid escape sequence at end of pattern: ${pattern}`);
+    } 
+    else if (pattern[i] === '[') {
+      let j = i + 1;
+      while (j < pattern.length && pattern[j] !== ']') {
+        j ++;
+      }
+      if (j >= pattern.length) {
+        throw new Error(`Unclosed character class starting at index ${i}: ${pattern}`);
+      }
+      tokens.push(pattern.slice(i, j+1));
+      i = j+1;
+    }
+    else {
+      tokens.push(pattern[i]);
+      i++;
+    }
+  }
+
+  return tokens;
+}
 
 //function to check if a single character matches a pattern token.
 
 //function to iterate through the input line and the parsed pattern to check for a match.
-
 
 
 function main() {
